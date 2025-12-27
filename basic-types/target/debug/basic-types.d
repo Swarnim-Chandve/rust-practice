2ad7f8b2d0b06512598dbad0cf9520e58cf0f02e
swarnim <swarnimchandve23@gmail.com>
2025-12-27 16:47:55 +0530

    basic types(numbers)

---
+++ basic-types/target/debug/basic-types.d
@@ -0,0 +1,1 @@
+/home/swarnim/Desktop/hackathons/rust-practice/basic-types/target/debug/basic-types: /home/swarnim/Desktop/hackathons/rust-practice/basic-types/src/main.rs
---
+++ basic-types/target/debug/deps/basic_types-be3044e0b0e745a2.d
@@ -0,0 +1,5 @@
+/home/swarnim/Desktop/hackathons/rust-practice/basic-types/target/debug/deps/basic_types-be3044e0b0e745a2.d: src/main.rs
+
+/home/swarnim/Desktop/hackathons/rust-practice/basic-types/target/debug/deps/basic_types-be3044e0b0e745a2: src/main.rs
+
+src/main.rs:
